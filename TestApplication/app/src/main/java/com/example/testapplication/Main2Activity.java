a9ca5d55887a7dcea5bb8630e93cc0154b6892b6
waterandwind <112683966@qq.com>
2020-02-16 14:54:22 +0800

    恢复

--- TestApplication/app/src/main/java/com/example/testapplication/Main2Activity.java
+++ TestApplication/app/src/main/java/com/example/testapplication/Main2Activity.java
@@ -31,14 +31,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.testapplication.Adapters.CarAdapter;
-import com.example.testapplication.Bean.FoodBean;
-import com.example.testapplication.Fragments.FirstFragment;
-import com.example.testapplication.Utils.HttpUtil;
-import com.example.testapplication.Utils.UrlString;
-import com.example.testapplication.Utils.ViewUtils;
-import com.example.testapplication.View.AddWidget;
-import com.example.testapplication.View.ShopCarView;
+
 import com.shizhefei.view.indicator.IndicatorViewPager;
 import com.shizhefei.view.indicator.ScrollIndicatorView;
 import com.shizhefei.view.indicator.slidebar.ColorBar;
@@ -50,17 +43,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Main2Activity extends BaseActivity implements AddWidget.OnAddClick  {
-    private DrawerLayout mDrawerLayout;
-    private ImageView imageView;
-    private ShopCarView shopCarView;
-    private FirstFragment firstFragment;
-    public static CarAdapter carAdapter;
-    public static final String CAR_ACTION = "handleCar";
-    public static final String CLEARCAR_ACTION = "clearCar";
-    private CoordinatorLayout rootview;
-    public BottomSheetBehavior behavior;
-    public View scroll_container;
+public class Main2Activity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,182 +86,182 @@ public class Main2Activity extends BaseActivity implements AddWidget.OnAddClick 
 //            }
 //        });
     }
-    @Override
-    public void onAddClick(View view, FoodBean fb) {
-        dealCar(fb);
-        ViewUtils.addTvAnim(view, shopCarView.carLoc, mContext, rootview);
-    }
-
-    @Override
-    public void onSubClick(FoodBean fb) {
-        dealCar(fb);
-    }
-
-    private void dealCar(FoodBean foodBean) {
-        HashMap<String, Long> typeSelect = new HashMap<>();//更新左侧类别badge用
-        BigDecimal amount = new BigDecimal(0.0);
-        int total = 0;
-        boolean hasFood = false;
-        if (behavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
-            firstFragment.getFoodAdapter().notifyDataSetChanged();
-        }
-        List<FoodBean> flist = carAdapter.getData();
-        int p = -1;
-        for (int i = 0; i < flist.size(); i++) {
-            FoodBean fb = flist.get(i);
-            if (fb.getId() == foodBean.getId()) {
-                fb = foodBean;
-                hasFood = true;
-                if (foodBean.getSelectCount() == 0) {
-                    p = i;
-                } else {
-                    carAdapter.setData(i, foodBean);
-                }
-            }
-            total += fb.getSelectCount();
-            if (typeSelect.containsKey(fb.getType())) {
-                typeSelect.put(fb.getType(), typeSelect.get(fb.getType()) + fb.getSelectCount());
-            } else {
-                typeSelect.put(fb.getType(), fb.getSelectCount());
-            }
-            amount = amount.add(fb.getPrice().multiply(BigDecimal.valueOf(fb.getSelectCount())));
-        }
-        if (p >= 0) {
-            carAdapter.remove(p);
-        } else if (!hasFood && foodBean.getSelectCount() > 0) {
-            carAdapter.addData(foodBean);
-            if (typeSelect.containsKey(foodBean.getType())) {
-                typeSelect.put(foodBean.getType(), typeSelect.get(foodBean.getType()) + foodBean.getSelectCount());
-            } else {
-                typeSelect.put(foodBean.getType(), foodBean.getSelectCount());
-            }
-            amount = amount.add(foodBean.getPrice().multiply(BigDecimal.valueOf(foodBean.getSelectCount())));
-            total += foodBean.getSelectCount();
-        }
-        shopCarView.showBadge(total);
-        firstFragment.getTypeAdapter().updateBadge(typeSelect);
-        shopCarView.updateAmount(amount);
-    }
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_main2;
-    }
-    private void initViews() {
-        rootview =  findViewById(R.id.rootview);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        initViewpager();
-        initShopCar();
-    }
-    private void initViewpager() {
-        scroll_container = findViewById(R.id.scroll_container);
-        ScrollIndicatorView mSv = (ScrollIndicatorView) findViewById(R.id.indicator);
-        ColorBar colorBar = new ColorBar(mContext, ContextCompat.getColor(mContext, R.color.dicator_line_blue), 6,
-                ScrollBar.Gravity.BOTTOM);
-        colorBar.setWidth(ViewUtils.dip2px(mContext, 30));
-        mSv.setScrollBar(colorBar);
-        mSv.setSplitAuto(false);
-        mSv.setOnTransitionListener(new OnTransitionTextListener().setColor(ContextCompat.getColor(mContext, R.color.dicator_line_blue),
-                ContextCompat.getColor(mContext, R.color.black)));
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        IndicatorViewPager indicatorViewPager = new IndicatorViewPager(mSv, mViewPager);
-        firstFragment = new FirstFragment();
-        ViewpagerAdapter myAdapter = new ViewpagerAdapter(getSupportFragmentManager());
-        indicatorViewPager.setAdapter(myAdapter);
-    }
-
-    private class ViewpagerAdapter extends IndicatorViewPager.IndicatorFragmentPagerAdapter {
-        private LayoutInflater inflater;
-        private int padding;
-        private String[] tabs = new String[]{"商品"};
-
-        ViewpagerAdapter(FragmentManager fragmentManager) {
-            super(fragmentManager);
-            inflater = LayoutInflater.from(mContext);
-            padding = ViewUtils.dip2px(mContext, 20);
-        }
-
-        @Override
-        public int getCount() {
-            return tabs.length;
-        }
-
-        @Override
-        public View getViewForTab(int position, View convertView, ViewGroup container) {
-            convertView = inflater.inflate(R.layout.item_textview, container, false);
-            TextView textView = (TextView) convertView;
-            textView.setText(tabs[position]); //名称
-            textView.setPadding(padding, 0, padding, 0);
-            return convertView;
-        }
-
-        @Override
-        public Fragment getFragmentForPage(int position) {
-            switch (position) {
-                case 0:
-                    return firstFragment;
-            }
-            return null;
-        }
-
-    }
-    private void initShopCar() {
-        behavior = BottomSheetBehavior.from(findViewById(R.id.car_container));
-        shopCarView = (ShopCarView) findViewById(R.id.car_mainfl);
-        View blackView = findViewById(R.id.blackview);
-        shopCarView.setBehavior(behavior, blackView);
-        RecyclerView carRecView = (RecyclerView) findViewById(R.id.car_recyclerview);
-//		carRecView.setNestedScrollingEnabled(false);
-        carRecView.setLayoutManager(new LinearLayoutManager(mContext));
-        ((DefaultItemAnimator) carRecView.getItemAnimator()).setSupportsChangeAnimations(false);
-        carAdapter = new CarAdapter(new ArrayList<FoodBean>(), this);
-        carAdapter.bindToRecyclerView(carRecView);
-    }
-
-
-    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            switch (intent.getAction()) {
-                case CAR_ACTION:
-                    FoodBean foodBean = (FoodBean) intent.getSerializableExtra("foodbean");
-                    FoodBean fb = foodBean;
-                    int p = intent.getIntExtra("position", -1);
-                    if (p >= 0 && p < firstFragment.getFoodAdapter().getItemCount()) {
-                        fb = firstFragment.getFoodAdapter().getItem(p);
-                        fb.setSelectCount(foodBean.getSelectCount());
-                        firstFragment.getFoodAdapter().setData(p, fb);
-                    } else {
-                        for (int i = 0; i < firstFragment.getFoodAdapter().getItemCount(); i++) {
-                            fb = firstFragment.getFoodAdapter().getItem(i);
-                            if (fb.getId() == foodBean.getId()) {
-                                fb.setSelectCount(foodBean.getSelectCount());
-                                firstFragment.getFoodAdapter().setData(i, fb);
-                                break;
-                            }
-                        }
-                    }
-                    dealCar(fb);
-                    break;
-                case CLEARCAR_ACTION:
-                    clearCar();
-                    break;
-            }
-            if (CAR_ACTION.equals(intent.getAction())) {
-
-            }
-        }
-    };
-
-    private void clearCar() {
-        List<FoodBean> flist = carAdapter.getData();
-        for (int i = 0; i < flist.size(); i++) {
-            FoodBean fb = flist.get(i);
-            fb.setSelectCount(0);
-        }
-        carAdapter.setNewData(new ArrayList<FoodBean>());
-        firstFragment.getFoodAdapter().notifyDataSetChanged();
-        shopCarView.showBadge(0);
-        firstFragment.getTypeAdapter().updateBadge(new HashMap<String, Long>());
-        shopCarView.updateAmount(new BigDecimal(0.0));
-    }
+//    @Override
+//    public void onAddClick(View view, FoodBean fb) {
+//        dealCar(fb);
+//        ViewUtils.addTvAnim(view, shopCarView.carLoc, mContext, rootview);
+//    }
+//
+//    @Override
+//    public void onSubClick(FoodBean fb) {
+//        dealCar(fb);
+//    }
+//
+//    private void dealCar(FoodBean foodBean) {
+//        HashMap<String, Long> typeSelect = new HashMap<>();//更新左侧类别badge用
+//        BigDecimal amount = new BigDecimal(0.0);
+//        int total = 0;
+//        boolean hasFood = false;
+//        if (behavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+//            firstFragment.getFoodAdapter().notifyDataSetChanged();
+//        }
+//        List<FoodBean> flist = carAdapter.getData();
+//        int p = -1;
+//        for (int i = 0; i < flist.size(); i++) {
+//            FoodBean fb = flist.get(i);
+//            if (fb.getId() == foodBean.getId()) {
+//                fb = foodBean;
+//                hasFood = true;
+//                if (foodBean.getSelectCount() == 0) {
+//                    p = i;
+//                } else {
+//                    carAdapter.setData(i, foodBean);
+//                }
+//            }
+//            total += fb.getSelectCount();
+//            if (typeSelect.containsKey(fb.getType())) {
+//                typeSelect.put(fb.getType(), typeSelect.get(fb.getType()) + fb.getSelectCount());
+//            } else {
+//                typeSelect.put(fb.getType(), fb.getSelectCount());
+//            }
+//            amount = amount.add(fb.getPrice().multiply(BigDecimal.valueOf(fb.getSelectCount())));
+//        }
+//        if (p >= 0) {
+//            carAdapter.remove(p);
+//        } else if (!hasFood && foodBean.getSelectCount() > 0) {
+//            carAdapter.addData(foodBean);
+//            if (typeSelect.containsKey(foodBean.getType())) {
+//                typeSelect.put(foodBean.getType(), typeSelect.get(foodBean.getType()) + foodBean.getSelectCount());
+//            } else {
+//                typeSelect.put(foodBean.getType(), foodBean.getSelectCount());
+//            }
+//            amount = amount.add(foodBean.getPrice().multiply(BigDecimal.valueOf(foodBean.getSelectCount())));
+//            total += foodBean.getSelectCount();
+//        }
+//        shopCarView.showBadge(total);
+//        firstFragment.getTypeAdapter().updateBadge(typeSelect);
+//        shopCarView.updateAmount(amount);
+//    }
+//    @Override
+//    protected int getLayoutId() {
+//        return R.layout.activity_main2;
+//    }
+//    private void initViews() {
+//        rootview =  findViewById(R.id.rootview);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        initViewpager();
+//        initShopCar();
+//    }
+//    private void initViewpager() {
+//        scroll_container = findViewById(R.id.scroll_container);
+//        ScrollIndicatorView mSv = (ScrollIndicatorView) findViewById(R.id.indicator);
+//        ColorBar colorBar = new ColorBar(mContext, ContextCompat.getColor(mContext, R.color.dicator_line_blue), 6,
+//                ScrollBar.Gravity.BOTTOM);
+//        colorBar.setWidth(ViewUtils.dip2px(mContext, 30));
+//        mSv.setScrollBar(colorBar);
+//        mSv.setSplitAuto(false);
+//        mSv.setOnTransitionListener(new OnTransitionTextListener().setColor(ContextCompat.getColor(mContext, R.color.dicator_line_blue),
+//                ContextCompat.getColor(mContext, R.color.black)));
+//        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+//        IndicatorViewPager indicatorViewPager = new IndicatorViewPager(mSv, mViewPager);
+//        firstFragment = new FirstFragment();
+//        ViewpagerAdapter myAdapter = new ViewpagerAdapter(getSupportFragmentManager());
+//        indicatorViewPager.setAdapter(myAdapter);
+//    }
+//
+//    private class ViewpagerAdapter extends IndicatorViewPager.IndicatorFragmentPagerAdapter {
+//        private LayoutInflater inflater;
+//        private int padding;
+//        private String[] tabs = new String[]{"商品"};
+//
+//        ViewpagerAdapter(FragmentManager fragmentManager) {
+//            super(fragmentManager);
+//            inflater = LayoutInflater.from(mContext);
+//            padding = ViewUtils.dip2px(mContext, 20);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return tabs.length;
+//        }
+//
+//        @Override
+//        public View getViewForTab(int position, View convertView, ViewGroup container) {
+//            convertView = inflater.inflate(R.layout.item_textview, container, false);
+//            TextView textView = (TextView) convertView;
+//            textView.setText(tabs[position]); //名称
+//            textView.setPadding(padding, 0, padding, 0);
+//            return convertView;
+//        }
+//
+//        @Override
+//        public Fragment getFragmentForPage(int position) {
+//            switch (position) {
+//                case 0:
+//                    return firstFragment;
+//            }
+//            return null;
+//        }
+//
+//    }
+//    private void initShopCar() {
+//        behavior = BottomSheetBehavior.from(findViewById(R.id.car_container));
+//        shopCarView = (ShopCarView) findViewById(R.id.car_mainfl);
+//        View blackView = findViewById(R.id.blackview);
+//        shopCarView.setBehavior(behavior, blackView);
+//        RecyclerView carRecView = (RecyclerView) findViewById(R.id.car_recyclerview);
+////		carRecView.setNestedScrollingEnabled(false);
+//        carRecView.setLayoutManager(new LinearLayoutManager(mContext));
+//        ((DefaultItemAnimator) carRecView.getItemAnimator()).setSupportsChangeAnimations(false);
+//        carAdapter = new CarAdapter(new ArrayList<FoodBean>(), this);
+//        carAdapter.bindToRecyclerView(carRecView);
+//    }
+//
+//
+//    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            switch (intent.getAction()) {
+//                case CAR_ACTION:
+//                    FoodBean foodBean = (FoodBean) intent.getSerializableExtra("foodbean");
+//                    FoodBean fb = foodBean;
+//                    int p = intent.getIntExtra("position", -1);
+//                    if (p >= 0 && p < firstFragment.getFoodAdapter().getItemCount()) {
+//                        fb = firstFragment.getFoodAdapter().getItem(p);
+//                        fb.setSelectCount(foodBean.getSelectCount());
+//                        firstFragment.getFoodAdapter().setData(p, fb);
+//                    } else {
+//                        for (int i = 0; i < firstFragment.getFoodAdapter().getItemCount(); i++) {
+//                            fb = firstFragment.getFoodAdapter().getItem(i);
+//                            if (fb.getId() == foodBean.getId()) {
+//                                fb.setSelectCount(foodBean.getSelectCount());
+//                                firstFragment.getFoodAdapter().setData(i, fb);
+//                                break;
+//                            }
+//                        }
+//                    }
+//                    dealCar(fb);
+//                    break;
+//                case CLEARCAR_ACTION:
+//                    clearCar();
+//                    break;
+//            }
+//            if (CAR_ACTION.equals(intent.getAction())) {
+//
+//            }
+//        }
+//    };
+//
+//    private void clearCar() {
+//        List<FoodBean> flist = carAdapter.getData();
+//        for (int i = 0; i < flist.size(); i++) {
+//            FoodBean fb = flist.get(i);
+//            fb.setSelectCount(0);
+//        }
+//        carAdapter.setNewData(new ArrayList<FoodBean>());
+//        firstFragment.getFoodAdapter().notifyDataSetChanged();
+//        shopCarView.showBadge(0);
+//        firstFragment.getTypeAdapter().updateBadge(new HashMap<String, Long>());
+//        shopCarView.updateAmount(new BigDecimal(0.0));
+//    }
 }
